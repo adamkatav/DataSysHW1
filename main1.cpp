8f682a0961cc56c0d2507ad0f9ee8ec9313cfc4f
Adam Katav <adamkatav@gmail.com>
2022-04-21 10:53:38 +0300

    First commit

---
+++ main1.cpp
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en" dir="LTR" style="height: 100%">
+<head>
+
+<meta name="Description" content="The all-in-one site for the students. Here you can view your grades, homeworks, course announcements and courses homepage. It is a part of the WebCourse(tm) and GR++(tm) packages. A service by Yaniv Hamo (tm)">
+<meta name="Keywords" content="GR++, Yaniv Hamo (tm), WebCourse, Yaniv, Hamo, Yaniv Hamo">
+<meta name="Author" content="Yaniv Hamo, Yaniv Hamo (tm)">
+<meta name="Generator" content="Yaniv Hamo (tm)">
+<meta name="Copyright" content="1999 Yaniv Hamo (tm)">
+<meta http-equiv="Cache-Control" content="max-age=900">
+
+  <!-- Tell the browser we use UTF-8: (should be first) -->
+  <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+
+  <!-- A work-arround for IE 11 and Edge to ignore compatibility mode (e.g., ignore request to behave as IE 7) -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <!-- Tiling support for Windows: -->
+  <meta name="msapplication-TileColor" content="#752d75">
+  <meta name="msapplication-square70x70logo" content="tiny.png">
+  <meta name="msapplication-square150x150logo" content="square.png">
+  <meta name="msapplication-wide310x150logo" content="wide.png">
+  <meta name="msapplication-square310x310logo" content="large.png">
+  <link rel="stylesheet" type="text/css" href="/css/gr.css?566">
+  <script src="/js/all.js"></script>
+
+  <title>Grades Login Screen - Web based grades system</title>
+
+<!--Solved img location issues. See https://developer.mozilla.org/en-US/docs/Images,_Tables,_and_Mysterious_Gaps-->
+<style>
+td img {display: block;}
+</style>
+
+</head>
+<body class="gr-stud gr-stud-login" onload="document.grform.ID.focus();">
+
+<div class="top-div">
+<form name="grform" action="/grades.cgi" method="post">
+<div class="main">
+<input type="hidden" name="Login" value="1">
+<input type="hidden" name="Course" value="">
+<input type="hidden" name="Page" value="">
+<input type="hidden" name="SEM" value="">
+<div style="text-align: center; width: 644px; margin-bottom: 0px; margin-top: 2px; background-color: #f0f0f0; border-radius: 25px 25px 0 0; padding: 5px 25px; direction: rtl;">
+<span class="red-text"><strong>You were automatically logged out after 120 minutes<br><img src="/Images/StudImages/spacer.gif" height="5" alt=""></strong></span>
+
+</div>
+<div class="taub">
+  <div class="taub-top-img">
+      <img src="/Images/StudImages/Taub/top.gif" width="693" height="9" alt="">
+  </div>
+  <div class="taub-top">
+    <span>
+      <img src="/Images/StudImages/Taub/gr%2b%2b.gif" alt="logo of GR++, the E grading solution"><span>
+<label for="ID">Username:</label> <input type="text" size="10" maxlength="250" id="ID" name="ID" value="" required aria-label="username שם משתמש" aria-invalid="false" aria-required="true">
+<label for="password">Password:</label> <input type="password" id="password" name="Password" size="10" maxlength="250" required aria-label="password סיסמה" aria-invalid="false" aria-required="true">
+<input type="submit" class="submit-button" value="proceed" name="submit">
+</span>
+<img src="/Images/StudImages/Taub/rightcorner.gif" alt="">
+    </span>
+  </div>
+  <div>
+      <img src="/Images/StudImages/Taub/images3/1.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/2.jpg" width="115" height="97" alt=""><img src="/Images/StudImages/Taub/images3/3.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/4.jpg" width="115" height="97" alt=""><img src="/Images/StudImages/Taub/images3/5.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/6.jpg" width="115" height="97" alt="">
+  </div>
+  <div>
+      <img src="/Images/StudImages/Taub/images3/7.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/8.jpg" width="115" height="97" alt=""><img src="/Images/StudImages/Taub/images3/9.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/10.jpg" width="115" height="97" alt=""><img src="/Images/StudImages/Taub/images3/11.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/12.jpg" width="115" height="97" alt="">
+  </div>
+  <div>
+      <img src="/Images/StudImages/Taub/images3/13.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/14.jpg" width="115" height="97" alt=""><img src="/Images/StudImages/Taub/images3/15.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/16.jpg" width="115" height="97" alt=""><img src="/Images/StudImages/Taub/images3/17.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/18.jpg" width="115" height="97" alt="">
+  </div>
+  <div>
+      <img src="/Images/StudImages/Taub/images3/19.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/20.jpg" width="115" height="97" alt=""><img src="/Images/StudImages/Taub/images3/21.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/22.jpg" width="115" height="97" alt=""><img src="/Images/StudImages/Taub/images3/23.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/24.jpg" width="115" height="97" alt="">
+  </div>
+  <div>
+      <img src="/Images/StudImages/Taub/images3/25.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/26.jpg" width="115" height="97" alt=""><img src="/Images/StudImages/Taub/images3/27.jpg" width="116" height="97" alt=""><img src="/Images/StudImages/Taub/images3/28.jpg" width="115" height="97" alt=""><img border="0" src="/Images/StudImages/Taub/images3/29.jpg" width="116" height="97" alt="Yaniv Hamo"><img border="0" src="/Images/StudImages/Taub/images3/30.jpg" width="115" height="97" alt="Yaniv Hamo">
+  </div>
+</div>
+</div>
+</form>
+</div>
+<div style="top: 0px; text-align: center;">
+<center>
+<div style="text-align: left; width: 644px; margin-bottom: 20px; margin-top: 0px; background-color: #f0f0f0; border-radius: 0 0 15px 15px; padding: 5px 25px; direction: rtl; font-size: 12px;">
+<span style="">
+<A HREF="/help.cgi">לעזרה</A> ומידע מפורט איך להיכנס למערכת לחץ <A HREF="/help.cgi">כאן</A>
+</span>
+<span style="float: right; right: 10px; position: relative;">
+<A HREF="https://techwww.technion.ac.il/cgi-bin/newuser/newuser.pl" TARGET="_BLANK">שכחתי סיסמא/החלפת סיסמא/פג תוקף סיסמא</A>
+</span>
+</div>
+
+<div style="text-align: justify; text-justify: inter-word; direction: rtl; width: 600px;">
+
+<center><h1 style="color: brown;">ברוכים הבאים למערכת <FONT DIR="LTR">GR++</FONT></h1></center>
+
+<P><B>הכניסה למערכת זו היא בשימוש בסיסמא הטכניונית האחידה בלבד</B>.
+זו אותה סיסמא כמו של
+t2, tx ו-Upgrade ושל דואל ב-campus.
+זה <B>לא</B> הקוד הסודי (בעל שמונה הספרות שמשמש לפתיחת חשבון ול-Moodle).
+סטודנטים שאינם מצליחים להיכנס יכולים לפנות 
+<a href="http://5600.technion.ac.il/"  target="_new">למוקד תמיכה טכניוני</a>
+או
+בדואל
+ל-<SPAN class="wclink-email" data-edn="pqscs.technion.ac.il" data-eun="ahjwwwb">usaratcs.technion.ac.il</SPAN>.
+אם קיבלתם הודעה על פג תוקף סיסמא, אנא החליפו סיסמא (שירות בדיקת הסיסמא שאנו מקבלים מהטכניון בודק פג תוקף, כך שגם אם במערכות טכניוניות אחרות אתם מסוגלים להשתמש בסיסמא למרות שפג תוקפה לא תוכלו להיכנס למערכת זו ללא שינוי הסיסמא).
+מצורף הסבר
+<a href="https://cis.technion.ac.il/kb/faq/services-identification/%d7%a2%d7%93%d7%9b%d7%95%d7%9f-%d7%95%d7%a9%d7%97%d7%96%d7%95%d7%a8-%d7%a1%d7%99%d7%a1%d7%9e%d7%94-%d7%90%d7%99%d7%a9%d7%99%d7%aa-%d7%91%d7%9e%d7%a7%d7%a8%d7%94-%d7%a9%d7%9c-%d7%a4%d7%a7%d7%99%d7%a2/" target="_new">לגבי עדכון ושחזור סיסמה אישית במקרה של פקיעת תוקף או אובדן.
+</a>
+</P>
+
+
+
+
+
+<P>הכניסה למערכת מותרת רק למשתמשים החוקיים. אין להשתמש בסקריפטים או מערכות אוטומטיות לכניסה למערכת, ואין לאפשר כניסה לשום גורם שאינו בעל החשבון עצמו.
+</P>
+</div>
+
+
+
+<div style="text-align: right;width: 600px;direction: rtl; background-color: #ffff80;border-radius: 10px;border: 2px solid black;margin: 6px; padding: 10px;">
+<div style="font-size:15px; font-weight:bold;color:blue;  ">
+מה? התרגיל להיום?
+<BR>
+קמת בבוקר ושכחת שיום מתכונת?
+<BR>
+איפה השיעור היום?
+<BR>
+חדש: היומן של <FONT DIR="LTR">GR++</FONT> איתך באייפון!
+<BR>
+<BR>
+</div>
+<DIV style="font-size:14px; color:black; font-weight: normal;">
+
+<FONT DIR="LTR">GR++</FONT> 
+
+מייצא עבורכם יומן אישי ליומן הסלולרי או ל-outlook שלכם, כולל אירועי המקצועות אליהם אתם רשומים (למשל מבחנים), זמני הגשת תרגילי בית, והשיעורים והתרגולים שלכם.
+<BR>
+<BR>
+הגדרת היומן תחת "הגדרות"->"עדכון אוטומטי" ב-grades.cs.technion.ac.il. הוראות התחברות מפורטות מופיעות <A HREF="/help.cgi#calendar">בעזרה</A>.
+<BR>
+<IMG SRC="/Images/Help/cal.png" ALT="example of calendar event of a lecture in course 234112 - Introduction to computer - C language by Dr. Elena Novbari" style="border: 1px solid black; border-radius: 7px;">
+
+</div>
+</div>
+
+
+
+</center>
+</div>
+
+<!-- DIV style="position: absolute; top:0; left:0; width: 20%; color: blue; background-color: #ffff80; border-radius: 10px; min-height: 50px; border: 2px solid black; margin: 6px; padding: 10px; font-size: 15px; font-weight: bold; direction: rtl; text-align: rtl; z-index: -1;" -->
+</body>
+</html>
