b16761ddcea4c83513f75dfa865a87dfad060a28
Mayan Amer <mayan.amer@outlook.com>
2022-04-22 12:08:32 +0300

    started EM and about to pull avl

--- employeeManager.cpp
+++ employeeManager.cpp
@@ -12,12 +12,27 @@ EmployeeManager::EmployeeManager():
    std::make_shared<AVLTree<EmployeeKey, Employee, std::weak_ptr>>(AVLTree<EmployeeKey, Employee, std::weak_ptr>()
    )))){}
 
-void *Init()
+StatusType EmployeeManager::AddCompany(int CompanyID, int Value)
 {
-   EmployeeManager manager();
+   if (CompanyID<=0 || Value<=0) return INVALID_INPUT;
+   std::shared_ptr<Company> company = companies->getValue(CompanyID);
+   if (company != nullptr) return FAILURE;
+
+   company = std::make_shared<Company>(Company(Value, CompanyID, nullptr,
+   std::make_shared<AVLTree<EmployeeKey, Employee, std::weak_ptr>>(AVLTree<EmployeeKey, Employee, std::weak_ptr>()));
+   if(company == nullptr) return ALLOCATION_ERROR;
+
+   companies->add(CompanyID,company);
+   return SUCCESS;
 }
 
-StatusType AddCompany(void *DS, int CompanyID, int Value)
+StatusType EmployeeManager::AddEmployee(int EmployeeID, int CompanyID, int Salary, int Grade)
 {
-   
+   if(EmployeeID<=0 || CompanyID<=0 || Salary<=0 || Grade<=0) return INVALID_INPUT;
+   std::weak_ptr<Company> company = companies->getValue(CompanyID);
+   if(company == nullptr) return FAILURE;
+   std::shared_ptr<Employee> employee = employees->getValue(EmployeeID);
+   if (employee != nullptr) return FAILURE;
+   employee = std::make_shared<Employee>(EmployeeID,Salary,Grade,company,this->dummy);
+
 }
--- employeeManager.h
+++ employeeManager.h
@@ -4,6 +4,7 @@
 #include "avlTree.h"
 #include "employee.h"
 #include "company.h"
+#include "library1.h"
 
 class EmployeeManager
 {
@@ -11,8 +12,22 @@ class EmployeeManager
     std::unique_ptr<AVLTree<int, Company, std::shared_ptr>> companies;
     std::unique_ptr<AVLTree<int, Employee, std::shared_ptr>> employees;
     std::shared_ptr<Company> dummy;
-
     EmployeeManager();
+    StatusType AddCompany(int CompanyID, int Value);
+    StatusType AddEmployee(int EmployeeID, int CompanyID, int Salary, int Grade);
+    StatusType RemoveCompany(int CompanyID);
+    StatusType RemoveEmployee(int EmployeeID);
+    StatusType GetCompanyInfo(int CompanyID, int *Value, int *NumEmployees);
+    StatusType GetEmployeeInfo(int EmployeeID, int *EmployerID, int *Salary, int *Grade);
+    StatusType IncreaseCompanyValue(int CompanyID, int ValueIncrease);
+    StatusType PromoteEmployee(int EmployeeID, int SalaryIncrease, int BumpGrade);
+    StatusType HireEmployee(int EmployeeID, int NewCompanyID);
+    StatusType AcquireCompany(int AcquirerID, int TargetID, double Factor);
+    StatusType GetHighestEarner(int CompanyID, int *EmployeeID);
+    StatusType GetAllEmployeesBySalary(int CompanyID, int **Employees, int *NumOfEmployees);
+    StatusType GetHighestEarnerInEachCompany(int NumOfCompanies, int **Employees);
+    StatusType GetNumEmployeesMatching(int CompanyID, int MinEmployeeID, int MaxEmployeeId,
+            int MinSalary, int MinGrade, int *TotalNumOfEmployees, int *NumOfEmployees);
 };
 
 
