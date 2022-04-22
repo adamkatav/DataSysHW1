7477dafc046b61d292f8425550a988dd0465405b
Mayan Amer <mayan.amer@outlook.com>
2022-04-22 14:01:29 +0300

    added RemoveCompany

--- employeeManager.cpp
+++ employeeManager.cpp
@@ -8,18 +8,18 @@
 EmployeeManager::EmployeeManager(): 
    companies(std::unique_ptr<AVLTree<int,Company,std::shared_ptr>>(new AVLTree<int,Company,std::shared_ptr>())),
    employees(std::unique_ptr<AVLTree<int,Employee,std::shared_ptr>>(new AVLTree<int,Employee,std::shared_ptr>())),
-   dummy(std::unique_ptr<Company>(new Company(0,0,nullptr,
+   dummy(std::unique_ptr<Company>(new Company(0,0,std::weak_ptr<Employee>(),
    std::make_shared<AVLTree<EmployeeKey, Employee, std::weak_ptr>>(AVLTree<EmployeeKey, Employee, std::weak_ptr>()
    )))){}
 
 StatusType EmployeeManager::AddCompany(int CompanyID, int Value)
 {
    if (CompanyID<=0 || Value<=0) return INVALID_INPUT;
-   std::shared_ptr<Company> company = companies->getValue(CompanyID);
+   std::shared_ptr<Company> company = std::make_shared<Company>(companies->getValue(CompanyID));
    if (company != nullptr) return FAILURE;
 
-   company = std::make_shared<Company>(Company(Value, CompanyID, nullptr,
-   std::make_shared<AVLTree<EmployeeKey, Employee, std::weak_ptr>>(AVLTree<EmployeeKey, Employee, std::weak_ptr>()));
+   company = std::make_shared<Company>(Company(Value, CompanyID, std::weak_ptr<Employee>(),
+   std::make_shared<AVLTree<EmployeeKey, Employee, std::weak_ptr>>(AVLTree<EmployeeKey, Employee, std::weak_ptr>())));
    if(company == nullptr) return ALLOCATION_ERROR;
 
    companies->add(CompanyID,company);
@@ -29,10 +29,60 @@ StatusType EmployeeManager::AddCompany(int CompanyID, int Value)
 StatusType EmployeeManager::AddEmployee(int EmployeeID, int CompanyID, int Salary, int Grade)
 {
    if(EmployeeID<=0 || CompanyID<=0 || Salary<=0 || Grade<=0) return INVALID_INPUT;
-   std::weak_ptr<Company> company = companies->getValue(CompanyID);
-   if(company == nullptr) return FAILURE;
-   std::shared_ptr<Employee> employee = employees->getValue(EmployeeID);
-   if (employee != nullptr) return FAILURE;
-   employee = std::make_shared<Employee>(EmployeeID,Salary,Grade,company,this->dummy);
 
+   std::weak_ptr<Company> company = companies->getValue(CompanyID);
+   if(company.lock() == nullptr) return FAILURE;
+
+   std::shared_ptr<Employee> employee = std::make_shared<Employee>(employees->getValue(EmployeeID));
+   if (employee != nullptr) return FAILURE;
+
+   employee = std::make_shared<Employee>(EmployeeID,Salary,Grade,company,this->dummy);
+   if (employee==nullptr) return ALLOCATION_ERROR;
+
+   employees->add(EmployeeID,employee); //add the employee to the general employee tree 
+
+   EmployeeKey current_key = EmployeeKey(EmployeeID,Salary);
+   company.lock()->employees->add(current_key,employee); //add the employee to its company
+
+   //check if we need to replace highest earner in company
+   if ( company.lock()->highest_earner.lock() == nullptr ) 
+   {
+      company.lock()->highest_earner = employee;
+   }
+   else
+   {
+      EmployeeKey old_highest_key = EmployeeKey(company.lock()->highest_earner.lock()->id, company.lock()->highest_earner.lock()->salary);
+      if( old_highest_key < current_key ) 
+      {
+         company.lock()->highest_earner = employee;
+      }
+   }
+
+   dummy->employees->add(current_key,employee); //add the employee to dummy
+
+   //check if the highest earner in dummy needs to be replaced
+   if ( dummy->highest_earner.lock() == nullptr ) 
+   {
+      dummy->highest_earner = employee;
+   }
+   else
+   {
+      EmployeeKey old_highest_key_dummy = EmployeeKey(dummy->highest_earner.lock()->id, dummy->highest_earner.lock()->salary);
+      if( old_highest_key_dummy < current_key ) 
+      {
+         dummy->highest_earner = employee;
+      }
+   }
+
+   return SUCCESS;
+}
+
+StatusType EmployeeManager::RemoveCompany(int CompanyID)
+{
+   if ( companies <= 0 ) return INVALID_INPUT;
+   std::weak_ptr<Company> company = companies->getValue(CompanyID);
+   if(company.lock() == nullptr) return FAILURE;
+   if( !company.lock()->employees->isEmpty()) return FAILURE;
+   companies->remove(CompanyID);
+   return SUCCESS;
 }
